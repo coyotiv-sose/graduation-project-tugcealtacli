f636e829a801ba57967141f6e2a560bb5330dca7
tugcealtacli <tugcealtacli@icloud.com>
2026-03-10 03:25:22 +0000

    Convert task reporter class to a utility function

--- src/task-reporter.js
+++ src/task-reporter.js
@@ -1,13 +1,11 @@
 const colors = require('colors')
 const pluralize = require('pluralize')
 
-class TaskReporter {
-  static printFullReport(task) {
-    const count = task.assignees.length
-    const employeeText = pluralize('employee', count, true)
-    const reportText = `${task.report}\n${task.title} projesinde ${employeeText} çalışıyor.`
-    console.log(reportText.magenta)
-  }
+const printFullReport = task => {
+  const count = task.assignees ? task.assignees.length : 0
+  const employeeText = pluralize('employee', count, true)
+  const reportText = `${task.report}\n${task.title} projesinde ${employeeText} çalışıyor.`
+  console.log(reportText.magenta)
 }
 
-module.exports = TaskReporter
+module.exports = { printFullReport }
