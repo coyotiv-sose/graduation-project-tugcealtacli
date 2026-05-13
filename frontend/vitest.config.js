3560c98f0c61c22a4544c14c40a5f2d8d8f38571
Tuğçe Altaçlı <tugcealtacli@Tugce-Dizustu-Bilgisayar.local>
2026-05-13 20:36:13 +0300

    Update vite.config.js for Cloud Run allowedHosts

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -1,17 +1,24 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    server: {
-      allowedHosts: true 
-    },
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-    },
-  }),
-)
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: true,
+    allowedHosts: true
+  },
+  preview: {
+    host: true,
+    allowedHosts: true
+  }
+})
